26a91dec2ad7ea7b60cad209eb015f15d30e9e21
An0pka <an0pka32@gmail.com>
2023-05-22 21:13:39 +0300

    Task 40

--- src/DigitSplitter.java
+++
@@ -1,8 +0,0 @@
-public class DigitSplitter {
-    public int getFirstDigit(int number){
-        return number / 10;
-    }
-    public int getSecondDigit(int number){
-                return number % 10;
-    }
-}
---
+++ src/ExonNames.java
@@ -0,0 +1,35 @@
+public class ExonNames {
+    public boolean areNamesEqual(String name1, String name2) {
+        return name1.equals(name2);
+    }
+
+    public String makeFullName(String firstName, String lastName) {
+        return firstName.toLowerCase() + " " + lastName.toUpperCase();
+    }
+
+    public boolean isNameLucky(String name) {
+        String lowerCaseName = name.toLowerCase();
+        return lowerCaseName.contains("a") || lowerCaseName.contains("o");
+    }
+
+    //Test output
+    public static void main(String[] args) {
+        ExonNames names = new ExonNames();
+
+        //Should be true
+        boolean namesEqual = names.areNamesEqual("nm", "nm");
+        System.out.println("names.areNamesEqual(\"nm\", \"nm\") = " + namesEqual);
+
+        //Should be "exor BIGO"
+        String fullName = names.makeFullName("exor", "bigo");
+        System.out.println("names.makeFullName(\"exor\", \"bigo\") = " + fullName);
+
+        //Should be true
+        boolean isNameLucky = names.isNameLucky("Bigo");
+        System.out.println("names.isNameLucky(\"Bigo\") = " + isNameLucky);
+
+        //Should be BA
+//        String nameCode = names.getNameCode("boRA");
+//        System.out.println("names.getNameCode(\"boRA\") = " + nameCode);
+        }
+}

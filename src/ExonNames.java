1f4f25df9f793daadbe1e69e8522e370dea16ad0
An0pka <an0pka32@gmail.com>
2023-05-23 21:31:08 +0300

    modul 3  task 3

--- src/ExonNames.java
+++ src/ExonNames.java
@@ -11,10 +11,39 @@ public class ExonNames {
         String lowerCaseName = name.toLowerCase();
         return lowerCaseName.contains("a") || lowerCaseName.contains("o");
     }
+
     public String getNameCode(String name) {
+        String code = name.charAt(0) + String.valueOf(name.charAt(name.length() - 1));
+        return code.toUpperCase();
+    }
+
+    public boolean isMoneyName(String name) {
         char firstLetter = name.charAt(0);
-        char lastLetter = name.charAt(3);
-            return String.valueOf(firstLetter + lastLetter);
+        char lastLetter = name.charAt(name.length() - 1);
+
+        boolean firstLetterOk = firstLetter >= '0' && firstLetter <= '9';
+        boolean lastLetterOk = lastLetter >= '0' && lastLetter <= '9';
+
+        return firstLetterOk && lastLetterOk;
+    }
+
+    public boolean isInvisibleName(String name) {
+        return name.strip().isEmpty();
+    }
+
+    public String makeNamePositive(String name) {
+        return name
+                .replace("no", "yes")
+                .replace("No", "yes")
+                .replace("nO", "yes")
+                .replace("NO", "yes");
+    }
+
+    public String makeNameClean(String name) {
+        return "CLEAN" + name.strip() + "CLEAN";
+    }
+    public String makeHalfOfName(String name) {
+        return name.substring(0, name.length() / 2);
     }
 
     //Test output
@@ -36,5 +65,25 @@ public class ExonNames {
         //Should be BA
         String nameCode = names.getNameCode("boRA");
         System.out.println("names.getNameCode(\"boRA\") = " + nameCode);
+
+        //Should be true
+        boolean isMoneyName = names.isMoneyName("31Boss31");
+        System.out.println("names.isMoneyName(\"31Boss31\") = " + isMoneyName);
+
+        //Should be true
+        boolean isInvisible = names.isInvisibleName(" ");
+        System.out.println("names.isInvisibleName(\" \") = " + isInvisible);
+
+        //Should be YesMont
+        String positive = names.makeNamePositive("NoMont");
+        System.out.println("names.makeNamePositive(\"NoMont\") = " + positive);
+
+        //Should be CLEANdirtyCLEAN
+        String cleanName = names.makeNameClean(" dirty ");
+        System.out.println("names.makeNameClean(\" dirty \") = " + cleanName);
+
+        //Should be Gu
+        String halfOfName = names.makeHalfOfName("Gurk");
+        System.out.println("names.makeHalfOfName(\"Gurk\") = " + names.makeHalfOfName("Gurk"));
     }
 }

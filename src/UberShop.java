c00da50ef15f09e612a496e1a4d60b82e6cff5eb
An0pka <an0pka32@gmail.com>
2023-06-04 22:17:31 +0300

    modul 4 task 5

--- src/UberShop.java
+++ src/UberShop.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 public class UberShop {
     public void printPrices(float[] prices) {
-        for(float price: prices) {
+        for (float price : prices) {
             System.out.println(price + " jup.");
         }
     }
 
     public void multiplyPrices(float[] prices) {
-        for(int i = 0; i < prices.length; i++) {
+        for (int i = 0; i < prices.length; i++) {
             float price = prices[i];
 
             if (price < 1000) {
@@ -20,27 +20,87 @@ public class UberShop {
             prices[i] = price;
         }
     }
+
     public int[] findMinMaxPrices(int[] prices) {
-        int minP = 0;
-        int maxP = 0;
+        if (prices.length == 0) {
+            return new int[0];
+        }
+
+        int min = prices[0];
+        int max = prices[0];
+
+        for (int price : prices) {
+            if (price > max) {
+                max = price;
+            }
+
+            if (price < min) {
+                min = price;
+            }
+        }
+
+        if (min == max) {
+            return new int[]{min};
+        }
+
+        return new int[]{min, max};
+    }
+
+    public int getMinPriceCount(int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+
+        int min = prices[0];
         for (int i = 0; i < prices.length; i++) {
-            if (prices.length == 0) {
-                return new int[0];
-            } if (prices[i] > prices[i+1]) {
-                maxP = prices[i];
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] == min) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    public int[] removePrice(int[] prices, int toRemove) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        for(int i = 1; i < numbers.length; i += 2) {
+            System.out.println(numbers[i]); //Выведет 2, 4
+        }
+
+        int count = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] == toRemove) {
+                count++;
+            }
+        }
+        int prise[] = new int[prices.length - count];
+        int index = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] != toRemove) {
+                prise[index] = prices[i];
+                index++;
             }
 
         }
+        return prise;
     }
+
+
 
     //Test output
     public static void main(String[] args) {
         UberShop shop = new UberShop();
 
-        //Should be [50, 1500]
-        int[] prices = new int[] {100, 1500, 300, 50};
-        int[] minMax = shop.findMinMaxPrices(prices);
-        System.out.println(Arrays.toString(minMax));
+        //Should be [150, 200]
+        int[] prices = new int[]{150, 100, 200};
+        int toRemove = 100;
+        System.out.println(Arrays.toString(shop.removePrice(prices, toRemove)));
     }
-
 }

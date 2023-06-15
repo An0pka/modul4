57566fdba128b203622f9d02a56e86aa6ec4d41e
An0pka <an0pka32@gmail.com>
2023-06-15 20:43:22 +0300

    modul 6 task 5

--- src/SpaceportMessages.java
+++
@@ -1,5 +0,0 @@
-public class SpaceportMessages {
-    public static String generateLandingMessage(String shipName, String spaceport, String time){
-        return "Spaceship " + shipName + " landing at " + spaceport + ", time is" + time;
-    }
-}
--- src/SpaceshipCalc.java
+++
@@ -1,8 +0,0 @@
-public class SpaceshipCalc {
-    public final int calculateAirBalloons(int passengerCount) {
-        return passengerCount * 10;
-    }
-    public int calculateBookCount(int passengerCount){
-        return passengerCount * 2;
-    }
-}
